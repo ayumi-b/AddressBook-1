ab1284956bff367d6f1f8ccbdecb35e3e8bef5d5
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-02-11 11:26:56 -0600

    added login/registration functionalong with a logout button.  path to firebase also works correctly

--- public/js/main.js
+++ public/js/main.js
@@ -4,22 +4,101 @@
 function hello(){
  return 'world';
 };
+var $tbody      = $('tbody'),
+    firebaseUrl = "https://bookofcontacts.firebaseio.com",
+    fb          = new Firebase(firebaseUrl),
+    newFbUrl; 
+    
+//login/registration part....
 
-var firebaseUrl = "https://bookofcontacts.firebaseio.com/friends.json";
-var $tbody = $('tbody');
+if (fb.getAuth()) {
+  $('.login').remove();
+  $('.loggedIn').toggleClass('hidden');
 
+   newFbUrl = firebaseUrl + '/users/' + fb.getAuth().uid + '/data';
+  
+  $.get(newFbUrl + '/friends.json', function (res) {
+    Object.keys(res).forEach(function (uuid) {
+      addRowToTable(uuid, res[uuid]);
+    });
+  });
+ }
+//in order for those registering to work....//
+ $('#registerButton').click(function (event) {
+   event.preventDefault();
+   var $loginForm = $('#loginForm'),
+       email      = $('#userEmail').val(),
+       pass       = $('#userPassword').val(),
+       data       = {email: email, password: pass};
+   registerAndLogin(data, function (err, auth) {
+     if (err) {
+       $('.error').text(err);
+     } else {
+       location.reload(true);
+     }
+   });
+ });
+
+//in order for those loggin back in to work...//
+ $('#logInButton').click(function(event){
+   var $loginForm = $('#loginForm'),
+       email      = $('#userEmail').val(),
+       pass       = $('#userPassword').val(),
+       data       = {email: email, password: pass};
+
+   event.preventDefault();
+
+   fb.authWithPassword(data, function(err, auth){
+     if (err) {
+       $('.error').text(err);
+     } else {
+       location.reload(true);
+     }
+   });
+ });
+
+//log out of user....//
+$('.logout').click(function () {
+  fb.unauth();
+  location.reload(true);
+})
+
+//allows for the new registrant to stay logged in....//
+function registerAndLogin(obj, cb) {
+  fb.createUser(obj, function(err) {
+    if (!err) {
+      fb.authWithPassword(obj, function (err, auth){
+        if (!err) {
+          cb(null, auth);
+        } else {
+          cb(err);
+        }
+      });
+    } else {
+      cb(err);
+    }
+  });
+}
+
+//NOW, for the part after the login.....//
 //when page loads, add data from firebase to table
 $(document).ready(function setUpPage() {
   console.log("the page is set");
 
   $('.fillOutForm').hide();
 
-  $.get(firebaseUrl, function(res){
+  $.get(firebaseUrl +'/friends.json', function(res){
     Object.keys(res).forEach(function(uuid){
       addRowToTable(uuid, res[uuid]);
     });
 
   });
+
+  //function showForm() {
+    //$('.fillOutForm').toggle();
+  //}
+  //$('#pleaseAddMe').on('click', showForm);  
+
 
 //show contact form 
 $('#pleaseAddMe').on('click', function toggled() {
@@ -58,7 +137,7 @@ $('#sendMyInfo').on('click',  function(event) {
                                    phone: phone,
                                    email: email});
   
-  $.post(firebaseUrl, friendToAdd, function(res) {
+  $.post(newFbUrl + '/friends.json', friendToAdd, function(res) {
     $tr.attr('data-uuid', res.name);
     $('tbody').append($tr);
 
@@ -94,8 +173,8 @@ $('tbody').on('click', '#removeRow', function(evt){
   var $tr = $(evt.target).closest('tr');
   $tr.remove();
 
-  var uuid = $tr.data('uuid');
-  var url = "https://bookofcontacts.firebaseio.com/friends/" + uuid + '.json';
+  var uuid = $tr.data('uuid'),
+      url  = newFbUrl + '/friends/' + uuid + '.json';
   $.ajax(url, {type: 'DELETE'});
 });
 
